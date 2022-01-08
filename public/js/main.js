7641dbba2227fb78ed998041555f45cb861bc721
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-08 10:41:48 -0500

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,91 @@
+const t1 = document.querySelectorAll('.itm')
+const d1 = document.querySelectorAll('.dte')
+const s1 = document.querySelectorAll('.stat')
+const c1 = document.querySelectorAll('.fa-circle-notch'||'.fa-check-circle')
+let taskArr = Array.from(t1)
+let dateArr = Array.from(d1)
+let statArr = Array.from(s1)
+let circleArr = Array.from(c1)
+
+console.log('task ',taskArr,' date ',dateArr,' status ',statArr)
+
+const delTask = document.querySelectorAll('.fa-trash')
+//const compTask = document.querySelectorAll('.fa-circle-notch')
+
+Array.from(delTask).forEach((element)=>{
+    element.addEventListener('click', deleteTask)
+})
+
+circleArr.forEach((element)=>{
+    element.addEventListener('click', taskStatus) 
+})
+
+statArr.forEach((element)=> {
+
+    if(element.innerText=='false'){
+        element.parentNode.firstElementChild.className='fa fa-circle-notch'
+        element.parentNode.getElementsByClassName('itm')[0].id='off'
+    }else {
+        if(element.innerText=='true') {
+            element.parentNode.firstElementChild.className='fa fa-check-circle'
+            element.parentNode.getElementsByClassName('itm')[0].id='on'
+        }
+    }
+})
+
+////post////
+    // being done in the form action no method needed. 
+
+
+///get////
+        //get request are happening on page reload and set to fire after each post, put and delete
+
+
+////put////
+ async function taskStatus (){
+       const taskId1 = this.parentNode.childNodes[3].innerText
+       const iStatus1 = (this.parentNode.childNodes[9].innerText=='true')?false:true         
+        
+        try{
+            const response = await fetch('/taskStatus', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({  
+                'TaskID': taskId1,
+                'IStatus': iStatus1
+                
+              })            
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+
+}
+   
+////delete////
+async function deleteTask(){
+    const taskDel1 = this.parentNode.childNodes[3].innerText //this represents the span where the delete button is. Then it goes up to the parent of the span the <li>. Then goes to the child node of the <li>. Finally stores its text in taskDel.
+    console.log(this.parentNode.childNodes)
+      
+    try{
+        const response = await fetch('deleteTask', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+           // this is the body that will be sent to server.js if not hard coded
+            body: JSON.stringify({  
+              'TaskDel': taskDel1
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload() // reloads page triggering a new get request
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
---
+++ server.js
@@ -0,0 +1,82 @@
+const PORT = process.env.PORT || 8000
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+////EJS Setup////
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+////DB connection////
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'todoList' 
+
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+        
+ 
+////POST////
+    
+        app.post('/updateList', (request, response) => {
+            db.collection('listItems').insertOne({taskItem: request.body.taskItem||'empty', date: request.body.date||'empty', status: false})
+            .then(result => {
+                console.log('Task Added')
+                response.redirect('/')
+            })
+            .catch(error => console.error(error))
+        })
+    
+
+////GET////
+        app.get('/',(request, response)=>{
+            db.collection('listItems').find().toArray()
+            .then(data => {
+             //console.log(data) //uncomment to see data
+                response.render('index.ejs', { info: data })
+    // data from the render is passed into info variable (you can use any word)
+     
+            })
+            .catch(error => console.error(error))
+        })  
+    
+////PUT////
+
+app.put('/taskStatus', (request, response) => {
+    console.log(request.body)
+    db.collection('listItems').updateOne({taskItem: request.body.TaskID},{$set: {status: request.body.IStatus }})
+
+    .then(result => {
+        console.log('task updated log')
+        response.json('task updated response')
+
+    })
+    .catch(error => console.error(error))
+ 
+})
+
+
+
+////DELETE////
+app.delete('/deleteTask', (request, response) => {
+    db.collection('listItems').deleteOne({taskItem: request.body.TaskDel||null}) 
+    .then(result => {
+        console.log('Task Deleted')
+        response.json('Task Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+
+//// Listen method ////
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+        })
