eeded15cb7d686a16c58b7d8e05be4b4ef973d17
Jamal Hall <jamalhallwebdev@gmail.com>
2022-01-08 18:27:12 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 8000
 const express = require('express')
 const app = express()
-const MongoClient = require('mongodb').MongoClient
+//const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 ////EJS Setup////
